5720d6014543ad16ad52ee2aa3b83070bd0b7a74
Helena12354 <166733522+Helena12354@users.noreply.github.com>
2024-11-07 11:56:22 -0300

    Update informacoesGLobais.js

--- graficos/informacoesGLobais.js
+++ graficos/informacoesGLobais.js
@@ -1,4 +1,4 @@
-const url = 'https://helena12354.github.io/awawwaw/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
